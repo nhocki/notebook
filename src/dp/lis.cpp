94013f090755e887d26e6b66c34152df186ac796
Andrés Mejía <andmej@gmail.com>
2012-04-01 15:01:03 -0500

    Formatting on LIS.

--- src/dp/lis.cpp
+++ src/dp/lis.cpp
@@ -1,22 +1,25 @@
-#define INF 2<<30-1
+// Longest Increasing Subsequence.
+// O(n log n)
+
+const int INF  = 1 << 30 - 1;
 
 int main(){
   int n;
-  while(scanf("%d", &n)==1){
-    vector<long>S(n);
-    vector<long>M(n+1,INF);
-    for(int i=0;i<n;++i) scanf("%ld", &S[i]);
-    M[0]=0;
-    int _m = 0;
-    for(int i=0; i<S.size();++i){
-      int d = upper_bound(M.begin(),M.begin()+n,S[i]) - M.begin();
-      if(S[i]!=M[d-1]){
+  while(scanf("%d", &n) == 1){
+    vector<long> S(n);
+    vector<long> M(n+1, INF);
+    for (int i = 0; i < n; ++i) scanf("%ld", &S[i]);
+    M[0] = 0;
+    int m = 0;
+    for (int i = 0; i < S.size(); ++i){
+      int d = upper_bound(M.begin(), M.begin() + n, S[i]) - M.begin();
+      if (S[i] != M[d-1]){
         M[d] = S[i];
-        _m = max(_m,d);
+        m = max(m,d);
         parent[S[i]] = M[d-1];
       }
     }
-    printf("%d\n",max(1,_m));
+    printf("%d\n", max(1, m));
   }
   return 0;
 }
